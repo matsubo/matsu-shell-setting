5ed44fc9f53baa31c0543d8afd70d484014cfa29
Yuki Matsukura <yuki.matsukura@gree.net>
2013-05-07 11:33:09 +0900

    added git installer

---
+++ installer/git.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#sudo apt-get update
+#sudo apt-get install  libncurses5-dev cmake build-essential -y
+
+src_dir=~/archive/
+
+if [ ! -d $src_dir ];then
+  mkdir $src_dir
+fi
+
+cd $src_dir
+
+
+sudo aptitude install autoconf zlib1g-dev libssl-dev gettext
+
+git clone https://github.com/git/git.git
+cd git
+make configure
+./configure --with-expat
+make
+sudo make install
+
+rehash
+
+
