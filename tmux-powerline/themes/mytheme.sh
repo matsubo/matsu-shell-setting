0a5ac73d662d87a4f92e884ef7597ca14f9c1c2d
Yuki Matsukura <yuki.matsukura@gree.net>
2013-06-11 13:21:22 +0900

    to be lightweight

--- tmux-powerline/themes/mytheme.sh
+++ tmux-powerline/themes/mytheme.sh
@@ -23,12 +23,12 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 148 234" \
+		#"tmux_session_info 148 234" \
 		"hostname 33 0" \
 		#"ifstat 30 255" \
-		"ifstat_sys 30 255" \
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		"wan_ip 24 255" \
+		#"ifstat_sys 30 255" \
+		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+		#"wan_ip 24 255" \
 		"vcs_branch 29 253" \
 		#"vcs_compare 60 255" \
 		#"vcs_staged 64 255" \
@@ -49,9 +49,9 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"battery 137 127" \
 		"weather 37 255" \
 		#"xkb_layout 125 117" \
-		"date_day 235 136" \
-		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		#"date_day 235 136" \
+		#"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		#"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
