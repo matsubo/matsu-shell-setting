1872c663515f992df3cdac2b00d56a1f24168583
Yuki Matsukura <yuki_matsukura@metaps.com>
2016-01-06 23:58:19 +0900

    cleanup

--- benchmark/php/extension_benchmark.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'BenchmarkRunner.php';
-require_once 'target/ExtensionBenchmarks.php';
-
-
-$runner = new BenchmarkRunner('ExtensionBenchmarks');
-$runner->run(0, 100000, 5000);
-
-
